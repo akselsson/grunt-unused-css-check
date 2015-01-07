7b2eb3b27cc28466deeabd61b456574d63d4847a
Patrik Akselsson <pakselsson@ebay.com>
2015-01-07 23:09:23 +0100

    accept strings without spaces in js

--- test/js_test.js
+++ test/js_test.js
@@ -12,6 +12,12 @@ exports.html = {
     single_string_without_class: function(test) {
         test.expect(1);
         var actual = js('var a = \'class_1\';','fileName');
+        test.deepEqual(actual,['class_1']);
+        test.done();
+    },
+    single_string_with_spaces: function(test) {
+        test.expect(1);
+        var actual = js('var a = \'one two\';','fileName');
         test.deepEqual(actual,[]);
         test.done();
     },
