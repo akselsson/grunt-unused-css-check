1cd09594c5f2b6fb97bd904a90d767c498b80b8a
Patrik Akselsson <pakselsson@ebay.com>
2015-01-07 21:00:19 +0100

    failure now changes exit code

--- test/css_check_test.js
+++ test/css_check_test.js
@@ -11,26 +11,28 @@ execOptions = {
 
 exports.css_check = {
     pass: function(test) {
-        test.expect(1);
+        test.expect(2);
         exec('grunt css_check:pass',execOptions,function(error, stdout) {
             //grunt.file.write('test/expected/pass',stdout);
             var actual = stdout;
             var expected = grunt.file.read('test/expected/pass');
 
             test.equal(actual,expected,'should generate correct output');
+            test.equal(null,error);
             test.done();
 
         });
 
     },
     fail: function(test) {
-        test.expect(1);
+        test.expect(2);
         exec('grunt css_check:fail',execOptions,function(error, stdout) {
             //grunt.file.write('test/expected/fail',stdout);
             var actual = stdout;
             var expected = grunt.file.read('test/expected/fail');
 
             test.equal(actual,expected,'should generate correct output');
+            test.equal(3,error.code);
             test.done();
 
         });
